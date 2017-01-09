02af6be7df8904eba0b223a7c81fe70bf42bc2b4
ion <ionix@protonmail.com>
2017-01-09 17:25:26 +0100

    new sprites, updated and additions

---
+++ RogueLike/Inventory/Gold.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RogueLike
+{
+    class Gold
+    {
+        public Texture2D Texture { get; set; }
+        public int Amount { get; set; }
+    }
+}
